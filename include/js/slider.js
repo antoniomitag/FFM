73f63690ad9a1a928f5a32e0af3906bc4a72874c
antoniomitag <antonio.mitag@protonmail.ch>
2021-12-02 15:45:43 +0100

    Gallery update
    - refactored javascript
    - added more image descriptions
    - removed unnecessary function calls from the javascript and put it into html

--- include/js/slider.js
+++ include/js/slider.js
@@ -4,30 +4,31 @@ const images = {
   "Cloudchaser87-02": "The Winner takes it all - by CloudChaser87",
   "Cloudchaser87-04":
     "Captain Cosmos and the pose of the warrior - by CloudChaser87",
-  "Dravgvr-01": "",
-  "Dravgvr-02": "",
-  "Dravgvr-03": "",
-  "Dravgvr-04": "",
-  "Dravgvr-05": "",
-  "Dravgvr-06": "",
-  "Dravgvr-07": "",
-  "MorteTM-01": "",
-  "MorteTM-02": "",
-  "MorteTM-03": "",
-  "MorteTM-04": "",
-  "MorteTM-05": "",
-  "MorteTM-06": "",
-  "MorteTM-07": "",
-  "MorteTM-08": "",
-  "MorteTM-09": "",
-  "MorteTM-10": "",
-  "MorteTM-11": "",
-  "MorteTM-12": "",
-  "MorteTM-13": "",
-  "MorteTM-14": "",
-  "MorteTM-15": "",
-  "MorteTM-16": "",
-  "MorteTM-17": "",
+  "Dravgvr-01": "A game of Overseer says! - by Dravgvr",
+  "Dravgvr-02": "Overseer says: JUMP!  - by Dravgvr",
+  "Dravgvr-03": "Musical chares was a blast!  - by Dravgvr",
+  "Dravgvr-04": "Two chairs left - it's getting tense!  - by Dravgvr",
+  "Dravgvr-05": "Shameless advertisement.  - by Dravgvr",
+  "Dravgvr-06": "Bun's treasure hunt begins.  - by Dravgvr",
+  "Dravgvr-07": "AFKing on the stage.  - by Dravgvr",
+  "MorteTM-01": "Fireworks while waiting on rewards. - by MorteTM",
+  "MorteTM-02": "More musical chairs.  - by MorteTM",
+  "MorteTM-03": "Musical chairs arena.  - by MorteTM",
+  "MorteTM-04": "Only a few left.  - by MorteTM",
+  "MorteTM-05":
+    "NarwindilTM in the back making sure nobody misbehaves.  - by MorteTM",
+  "MorteTM-06": "Costumed website promotion. - by MorteTM",
+  "MorteTM-07": "I think I dropped my clip.  - by MorteTM",
+  "MorteTM-08": "Zedd being dapper as all hell.  - by MorteTM",
+  "MorteTM-09": "The Easter Egg hunt begins! -  - by MorteTM",
+  "MorteTM-10": "People reading the first clue.  - by MorteTM",
+  "MorteTM-11": "More people reading clues.  - by MorteTM",
+  "MorteTM-12": "You lose. Or do you?  - by MorteTM",
+  "MorteTM-13": "Poetry slam.  - by MorteTM",
+  "MorteTM-14": "Leviathan's singing voice is amazing.  - by MorteTM",
+  "MorteTM-15": "Hanging out.  - by MorteTM",
+  "MorteTM-16": "Moments before a baseball bat came out.  - by MorteTM",
+  "MorteTM-17": "Hi!  - by MorteTM",
   "ZeddtheRED-01": "Welcome Zedd - by ZeddtheRed",
   "ZeddtheRED-01": "Trading plans - by ZeddtheRed",
   "ZeddtheRED-02": "Crafting time in the shelter! - by ZeddtheRed",
@@ -39,33 +40,43 @@ const images = {
   "ZeddtheRED-07": "A round of applause for our host! - by ZeddtheRed",
 };
 
+let currImg = ""; // sets current image so the slider knows where to go on next/prev
+let imgString = ""; // sets current image string so we can get the data from the object
+const next = "next";
+let imageKeys = Object.keys(images); // array of filenames
+let imageDesc = Object.values(images); // array of descriptions
+
 /* OPEN IMAGE IN VIEWER */
 function viewBigger(image) {
   document.body.classList.toggle("overlay-on");
   document.getElementById("overlay").classList.remove("hide");
   document.getElementById("overlay-image").src = image.src;
   document.getElementById("overlay-text").innerHTML = image.dataset.title;
-  window.currImg = image.dataset.name;
+  window.currImg = image.dataset.name; // sets currImg to the one clicked
 }
 
 /*CLOSE VIEWER function*/
-
 function closeViewer() {
   document.body.classList.remove("overlay-on");
   document.getElementById("overlay").classList.add("hide");
 }
-/*CLOSE VIEWER ON ESC*/
-function closeBigger(keypressed) {
+
+/*Listen to keydown and close viewer on esc, or switch images on arrow keys*/
+
+function onKeyDown(keypressed) {
   if (keypressed.code === "Escape") {
     closeViewer();
+  } else if (document.body.classList.contains("overlay-on")) {
+    if (keypressed.code == "ArrowLeft") {
+      changeSlide(left);
+    } else if (keypressed.code == "ArrowRight") {
+      changeSlide(right);
+    }
   }
 }
-document.addEventListener("keydown", closeBigger);
 
-/*CLOSE VIEWER ON X */
-document.getElementById("close-button").addEventListener("click", function () {
-  closeViewer();
-});
+/* On keydown on document trigger function onKeyDown */
+document.addEventListener("keydown", onKeyDown);
 
 /*CLOSE ON CLICK OUTSIDE*/
 document.getElementById("overlay").addEventListener("click", closeOnClick);
@@ -75,22 +86,15 @@ function closeOnClick(event) {
     closeViewer();
   }
 }
-/*Generate gallery*/
 
-let imgString = "";
+/*Generate gallery*/
 for (const [name, title] of Object.entries(images)) {
   imgString += `<li><img class="click" data-title="${title}" data-name="${name}" onclick="viewBigger(this)" src="include/img/gallery/2021-11-28/${name}.jpg"/></li>`;
 }
 
 document.getElementById("gallery").innerHTML = imgString;
 
-let currImg = "";
-
 /*gallery scroll*/
-const next = "next";
-let imageKeys = Object.keys(images);
-let imageDesc = Object.values(images);
-
 function changeSlide(direction) {
   /*find this image index*/
   let dir = 1;
@@ -107,10 +111,3 @@ function changeSlide(direction) {
     document.getElementById("overlay-text").innerHTML = imageDesc[currKey];
   }
 }
-
-document.getElementById("left").addEventListener("click", function () {
-  changeSlide(left);
-});
-document.getElementById("right").addEventListener("click", function () {
-  changeSlide(right);
-});
